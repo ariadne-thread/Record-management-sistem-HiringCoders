0211c542a32ccd9f8a225ea99284a1786e20c1dc
ariadne-thread <camilacestari@live.com>
2021-07-24 20:33:14 -0300

    update page

---
+++ js/script.js
@@ -0,0 +1,103 @@
+/* Abrir e fechar o modal de Cliente */
+const btnClientes = document.querySelector('#btnClientes')
+const modalCliente = document.querySelector('#modalCliente')
+const fecharCliente = document.querySelector('#fecharCliente')
+
+btnClientes.addEventListener('click', () => {
+    modalCliente.classList.add('mostrar')
+})
+
+fecharCliente.addEventListener('click', () => {
+    modalCliente.classList.remove('mostrar')
+})
+
+/* Abrir e fechar o modal de Produto */
+const btnProdutos = document.querySelector('#btnProdutos')
+const modalProduto = document.querySelector('#modalProduto')
+const fecharProduto = document.querySelector('#fecharProduto')
+
+btnProdutos.addEventListener('click', () => {
+    modalProduto.classList.add('mostrar')
+})
+
+fecharProduto.addEventListener('click', () => {
+    modalProduto.classList.remove('mostrar')
+})
+
+/* Cadastrar Cliente */
+const cadCliente = document.querySelector('#cadCliente')
+
+cadCliente.addEventListener('click', () => {
+
+    let nome = document.querySelector('#nome').value
+    let email = document.querySelector('#email').value
+    let telefone = document.querySelector('#telefone').value
+    let cpf = document.querySelector('#cpf').value
+    let endereco = document.querySelector('#endereco').value
+    let cidade = document.querySelector('#cidade').value
+    let cep = document.querySelector('#cep').value
+    let estado = document.querySelector('#estado').value
+
+    let dadosCliente = {
+        
+        nome,
+        email,
+        telefone,
+        cpf,
+        endereco,
+        cidade,
+        cep,
+        estado
+
+    }
+
+    let convertDadosCliente = JSON.stringify(dadosCliente);
+
+    if (localStorage.idClienteAtual) {
+        localStorage.idClienteAtual = Number(localStorage.idClienteAtual) + 1;
+        localStorage.setItem(`Cliente: ${localStorage.idClienteAtual}`, convertDadosCliente);
+    }else {
+        localStorage.idClienteAtual = 1;
+        localStorage.setItem(`Cliente: ${localStorage.idClienteAtual}`, convertDadosCliente);
+    }
+
+    alert(`Customer: ${dadosCliente.nome} registered successfully!`);
+    document.querySelector('#formCliente').reset();
+
+})
+
+/* Cadastrar Produto */
+const cadProduto = document.querySelector('#cadProduto')
+
+cadProduto.addEventListener('click', () => {
+
+    let produto = document.querySelector('#produto').value
+    let codigo = document.querySelector('#codigo').value
+    let categoria = document.querySelector('#categoria').value
+    let quantidade = document.querySelector('#quantidade').value
+    let descricao = document.querySelector('#descricao').value
+
+    let dadosProduto = {
+
+        produto,
+        codigo,
+        categoria,
+        quantidade,
+        descricao
+
+    }
+
+    let convertDadosProduto = JSON.stringify(dadosProduto)    
+    
+    if (localStorage.idProdutoAtual) {
+        localStorage.idClienteAtual = Number(localStorage.idProdutoAtual) + 1;
+        localStorage.setItem(`Produto: ${localStorage.idProdutoAtual}`, convertDadosProduto);
+    }else {
+        localStorage.idProdutoAtual = 1;
+        localStorage.setItem(`Produto: ${localStorage.idProdutoAtual}`, convertDadosProduto);
+    }
+
+    alert(`Product: ${dadosProduto.produto} registered successfully!`);
+    document.querySelector('#formCliente').reset();
+
+})
